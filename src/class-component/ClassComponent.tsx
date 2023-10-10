a3aac1d1863b62a38c01a474724126de9d1cfb92
Irina Rusinova <irina.rusinova19@gmail.com>
2023-10-10 17:59:42 +0300

    feature: add class component with input and button

---
+++ src/class-component/ClassComponent.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" name="" id="" />
+        <button type="submit">Submit</button>
+      </div>
+    );
+  }
+}
+
+export default ClassComponent;
+
