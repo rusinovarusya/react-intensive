c35aa773150235973ac05370793b2729603f13ce
Irina Rusinova <irina.rusinova19@gmail.com>
2023-10-11 12:17:36 +0300

    feature: add basic lifecycle methods with just console.log to check order

--- src/class-component/ClassComponent.tsx
+++ src/class-component/ClassComponent.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 
-class ClassComponent extends React.Component {
+interface StateType { text: string };
+
+  class ClassComponent extends React.Component<{}, StateType> {  
   constructor(props: any) {
     super(props);
     this.state = { text: "" };
+
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    this.setState((state, props) => {
+      return { text: state.text + "some text" };
+    });
+
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(/*prevProps, prevState, snapshot*/) {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
   }
 
   render() {
     return (
       <div className="class-component">
+        text: {this.state.text}
         <form>
           <input type="text" name="inputText" id="inputText" />
           <button type="submit">Submit</button>
@@ -19,4 +40,3 @@ class ClassComponent extends React.Component {
 }
 
 export default ClassComponent;
-
